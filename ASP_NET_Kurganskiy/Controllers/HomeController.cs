f25dfd794349b5316aca5f87a2ad19e0d7e4df84
Notliver <akurganskii@gmail.com>
2019-11-11 20:26:30 +0300

    Add project files.

---
+++ ASP_NET_Kurganskiy/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ASP_NET_Kurganskiy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+        public HomeController(IConfiguration configuration) => _Configuration = configuration;
+        public IActionResult Index() => View();
+        public IActionResult GetConfig() => Content(_Configuration["CustomData"]);
+    }
+}
