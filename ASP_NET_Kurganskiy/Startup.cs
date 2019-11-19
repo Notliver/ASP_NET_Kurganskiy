16ae0e888d0396f535aa849d70d0845e86bd2ab2
Notliver <akurganskii@gmail.com>
2019-11-19 13:18:01 +0300

    Bug fixed

--- ASP_NET_Kurganskiy/Startup.cs
+++ ASP_NET_Kurganskiy/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using ASP_NET_Kurganskiy.Infrastructure.Conventions;
 
 namespace ASP_NET_Kurganskiy
 {
@@ -22,7 +23,7 @@ namespace ASP_NET_Kurganskiy
             services.AddMvc(
                 opt =>
                 {
-                    opt.Conventions.Add();
+                    //opt.Conventions.Add(new CustomControllerConvention());
                 });
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
