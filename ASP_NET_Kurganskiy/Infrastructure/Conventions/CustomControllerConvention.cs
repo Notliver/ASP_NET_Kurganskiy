4e72901080162fb1d5605c8747b8be9dce41e704
Notliver <akurganskii@gmail.com>
2019-11-19 13:14:22 +0300

    Example of work with ControllerConvention

---
+++ ASP_NET_Kurganskiy/Infrastructure/Conventions/CustomControllerConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace ASP_NET_Kurganskiy.Infrastructure.Conventions
+{
+    public class CustomControllerConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            //controller.ControllerName;
+        }
+    }
+}
