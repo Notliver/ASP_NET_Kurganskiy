3d3ccb8506f608f3116e5ecf6f55ad4426bd1c35
Notliver <akurganskii@gmail.com>
2019-11-23 00:29:24 +0300

    Base Entities

---
+++ ASP_NET_Kurganskiy.Domain/Entities/Base/NameEntity.cs
@@ -0,0 +1,9 @@
+﻿using ASP_NET_Kurganskiy.Domain.Entities.Base.Interfaces;
+
+namespace ASP_NET_Kurganskiy.Domain.Entities.Base
+{
+    public abstract class NameEntity : BaseEntity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
