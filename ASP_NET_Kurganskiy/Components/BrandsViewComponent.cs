e4204964c4c76eb23b179cd9bd26b5342a99cb5a
Notliver <akurganskii@gmail.com>
2019-11-23 02:28:48 +0300

    Visual components

---
+++ ASP_NET_Kurganskiy/Components/BrandsViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using ASP_NET_Kurganskiy.Infrastructure.Interfaces;
+using ASP_NET_Kurganskiy.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Kurganskiy.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        public readonly IProductData _ProductData;
+
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+
+
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() => _ProductData
+            .GetBrands()
+            .Select(brand => new BrandViewModel
+            {
+                Id = brand.Id,
+                Name = brand.Name,
+                Order = brand.Order
+            })
+            .OrderBy(brand => brand.Order)
+            .ToList();
+
+    }
+}
---
+++ ASP_NET_Kurganskiy/ViewModels/BrandViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ASP_NET_Kurganskiy.Domain.Entities.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Kurganskiy.ViewModels
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public string Name { get  ; set ; }
+        public int Id { get  ; set ; }
+        public int Order { get ; set ; }
+    }
+}
