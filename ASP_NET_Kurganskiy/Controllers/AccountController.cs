5e398f9d6e343c4caabda4d5301312024948ae17
Notliver <akurganskii@gmail.com>
2019-11-28 23:38:21 +0300

    Prepair for user auth. Create RegisterViewModel model

---
+++ ASP_NET_Kurganskiy/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Kurganskiy.Controllers
+{
+    public class AccountController : Controller
+    {
+ 
+    }
+}
