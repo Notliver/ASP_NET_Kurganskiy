a7c9ec13cc1cff6919772b12313256b5cb8857f0
Notliver <akurganskii@gmail.com>
2019-12-03 02:14:03 +0300

    Cart work

---
+++ ASP_NET_Kurganskiy/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_NET_Kurganskiy.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Kurganskiy.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+        public CartController(ICartService CartService) => _CartService = CartService;
+
+        public IActionResult Details() => View(_CartService.TransformFromCart());
+
+        public IActionResult AddToCart(int id)
+        {
+            _CartService.AddToCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult DecrimentFromCart(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult RemoveAll(int id)
+        {
+            _CartService.RemoveAll();
+            return RedirectToAction("Details");
+        }
+    }
+}
---
+++ ASP_NET_Kurganskiy/Infrastructure/Interfaces/ICartService.cs
@@ -0,0 +1,21 @@
+﻿using ASP_NET_Kurganskiy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_Kurganskiy.Infrastructure.Interfaces
+{
+    public interface ICartService
+    {
+        void AddToCart(int id);
+
+        void DecrementFromCart(int id);
+
+        void RemoveFromCart(int id);
+
+        void RemoveAll();
+
+        CartViewModel TransformFromCart();
+    }
+}
